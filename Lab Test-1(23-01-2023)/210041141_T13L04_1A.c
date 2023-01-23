3dd7e381b726b79079660bfb21bbce3d9be2d4c1
Nayeem Fardin <nfrohan110@gmail.com>
2023-01-23 09:55:15 +0600

    added comments for clarity

--- Lab Test-1(23-01-2023)/210041141_T13L04_1A.c
+++ Lab Test-1(23-01-2023)/210041141_T13L04_1A.c
@@ -15,24 +15,28 @@ int main(){
     int ballCount = 0, legalCount = 0, overs = 0, balls = 0;
     gets(playString);
     ballCount = strlen(playString);
+    //count legal balls
     for(int i = 0; i < ballCount; i++){
         if(isLegal(playString[i])){
             legalCount++;
         }
     }
+    //calculate complete overs
     overs = legalCount / 6;
     balls = legalCount - overs * 6;
-
+    //text if plural overs 
     if(overs > 1){
         if(balls == 0){printf("%d OVERS", overs);}
         if(balls == 1){printf("%d OVERS %d BALL", overs, balls);}
         if(balls > 1){printf("%d OVERS %d BALLS", overs, balls);}
     }
+    //text if a single over
     else if(overs == 1){
         if(balls == 0){printf("%d OVER", overs);}
         if(balls == 1){printf("%d OVER %d BALL", overs, balls);}
         if(balls > 1){printf("%d OVER %d BALLS", overs, balls);}
     }
+    //text if first over is incomplete
     else if(overs == 0){
         if(balls == 0){printf("0 OVERS 0 BALLS");}
         if(balls == 1){printf("%d BALL", balls);}
--- Lab Test-1(23-01-2023)/210041141_T15L04_1A.c
+++ Lab Test-1(23-01-2023)/210041141_T15L04_1A.c
@@ -2,8 +2,12 @@
 
 int main(){
     long long int num, toggleTarget, shifter, modNum;
+    //get number and which bit to flip
     scanf("%lld %lld", &num, &toggleTarget);
+    //shift bits
     shifter = 1 << toggleTarget;
+    //toggle target bit
     modNum = num ^ shifter;
+    
     printf("%lld", modNum);
 }
